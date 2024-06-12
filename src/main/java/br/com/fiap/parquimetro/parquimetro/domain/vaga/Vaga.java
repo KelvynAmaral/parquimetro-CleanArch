19f268d5f1d6cb350b9436d2747d95d0fab4121a
Kelvyn <dev.kelvynjohnson@gmail.com>
2024-06-12 19:09:07 -0300

    Dominios do negócio

---
+++ src/main/java/br/com/fiap/parquimetro/parquimetro/domain/vaga/Vaga.java
@@ -0,0 +1,44 @@
+package br.com.fiap.parquimetro.parquimetro.domain.vaga;
+
+import br.com.fiap.parquimetro.parquimetro.domain.veiculo.Veiculo;
+
+public class Vaga {
+
+    private long idVaga;
+    private boolean ocupada;
+    private Veiculo veiculo;
+
+
+    public Vaga(long idVaga, boolean ocupada, Veiculo veiculo) {
+        this.idVaga = idVaga;
+        this.ocupada = ocupada;
+        this.veiculo = veiculo;
+    }
+    public Vaga() {
+    }
+
+
+    public boolean isOcupada() {
+        return ocupada;
+    }
+
+    public void setOcupada(boolean ocupada) {
+        this.ocupada = ocupada;
+    }
+
+    public Veiculo getVeiculo() {
+        return veiculo;
+    }
+
+    public void setVeiculo(Veiculo veiculo) {
+        this.veiculo = veiculo;
+    }
+
+    public long getIdVaga() {
+        return idVaga;
+    }
+
+    public void setIdVaga(long idVaga) {
+        this.idVaga = idVaga;
+    }
+}
